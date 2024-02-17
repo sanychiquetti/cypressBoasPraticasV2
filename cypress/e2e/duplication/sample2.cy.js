93f324bdeb401a4717851d1e0e7b2243adcad6e9
sanychiquetti <sanymara77@hotmail.com>
2024-02-17 16:50:51 -0300

    Otimizando os testes com a inserção de uma array e iterando um teste para cada item da arry

--- cypress/e2e/duplication/sample2.cy.js
+++ cypress/e2e/duplication/sample2.cy.js
@@ -15,23 +15,16 @@ describe('Code duplication bad practice - repetitive tests', () => {
       .clear()
   })
 
-  it('searches for "reactjs"', () => {
-    cy.get('@searchField')
-      .type('reactjs{enter}')
+  const termsToSearchFor = ['reactjs', 'vuejs', "java"]
 
-    cy.wait('@getStories')
-
-    cy.get('.table-row')
-      .should('have.length', 100)
-  })
-
-  it('searches for "vuejs"', () => {
-    cy.get('@searchField')
-      .type('vuejs{enter}')
-
-    cy.wait('@getStories')
-
-    cy.get('.table-row')
-      .should('have.length', 100)
+  termsToSearchFor.forEach( term => {
+    it(`searches for "${term}"`, () => {
+      cy.search(term)
+  
+      cy.wait('@getStories')
+  
+      cy.get('.table-row')
+        .should('have.length', 100)
+    })
   })
 })
