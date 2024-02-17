3e5256dd8113154b306521729161af8709b043b5
sanychiquetti <sanymara77@hotmail.com>
2024-02-17 19:32:55 -0300

    Otimizando testes lentos

--- cypress/e2e/slow-tests/sample2.cy.js
+++ cypress/e2e/slow-tests/sample2.cy.js
@@ -1,12 +1,9 @@
 describe('Slow tests bad practice - click a link to visit a page', () => {
   it('does not enable signup button when passwords do not match', () => {
-    cy.visit('https://notes-serverless-app.com')
+    cy.visit('https://notes-serverless-app.com/signup')
 
-    cy.contains('.btn', 'Signup')
-      .click()
-
-    cy.get('#email').type('joe@example.com')
-    cy.get('#password').type('foobarbaz')
+    cy.get('#email').type(Cypress.env('user_email'))
+    cy.get('#password').type(Cypress.env('user_password'), { log: false })
     cy.get('#confirmPassword').type('foobahbaz')
 
     cy.contains('button', 'Signup')
