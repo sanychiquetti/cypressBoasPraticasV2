b9604c15ad479f326bc21917cd2ce8682a32ebc7
sanychiquetti <sanymara77@hotmail.com>
2024-02-17 18:13:31 -0300

    Eliminando testes flaky

--- cypress/e2e/flaky-test/sample.cy.js
+++ cypress/e2e/flaky-test/sample.cy.js
@@ -2,12 +2,8 @@ import { faker } from '@faker-js/faker'
 
 describe('Flaky tests bad practice', () => {
   beforeEach(() => {
+    cy.intercept('GET', '**/search**', { fixture: 'stories'})
     cy.visit('https://wlsf82-hacker-stories.web.app')
-
-    cy.contains('p','Loading ...')
-      .should('be.visible')
-    cy.contains('p','Loading ...')
-      .should('not.exist')
   })
 
   Cypress._.times(10, () => {
@@ -15,12 +11,6 @@ describe('Flaky tests bad practice', () => {
       Cypress._.times(6, () => {
         cy.search(faker.random.word())
       })
-
-      cy.contains('p','Loading ...')
-        .should('be.visible')
-      cy.contains('p','Loading ...')
-        .should('not.exist')
-
       cy.get('.last-searches button')
         .should('have.length', 5)
     })
