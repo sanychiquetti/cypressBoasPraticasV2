f0cbdb6514f3666a9c62dc13bfd0f6fa9179aaea
sanychiquetti <sanymara77@hotmail.com>
2024-02-17 20:02:49 -0300

    Eliminando testes com dependencia

--- cypress/e2e/dependent-tests/sample.cy.js
+++ cypress/e2e/dependent-tests/sample.cy.js
@@ -1,8 +1,6 @@
 describe('Dependent tests bad practice', () => {
   beforeEach(() => {
-    cy.visit('http://notes-serverless-app.com')
-
-    cy.get('.navbar-nav a:contains(Login)').click()
+    cy.visit('http://notes-serverless-app.com/login')
 
     cy.get('#email').type(Cypress.env('user_email'))
     cy.get('#password').type(Cypress.env('user_password'), { log: false })
@@ -11,26 +9,23 @@ describe('Dependent tests bad practice', () => {
     cy.contains('h1', 'Your Notes').should('be.visible')
   })
 
-  it('creates a note', () => {
+  it('CRUDs a note', () => {
     cy.contains('Create a new note').click()
-
     cy.get('#content').type('My note')
     cy.contains('Create').click()
 
-    cy.get('.list-group').should('contain', 'My note')
-  })
+    cy.get('.list-group')
+      .should('contain', 'My note')
+      .click()
 
-  it('edits a note', () => {
-    cy.get('.list-group').contains('My note').click()
     cy.get('#content').type(' updated')
     cy.contains('Save').click()
 
     cy.get('.list-group').should('contain', 'My note updated')
-    cy.get('.list-group:contains(My note updated)').should('be.visible')
-  })
+    cy.get('.list-group:contains(My note updated)')
+      .should('be.visible')
+      .click()
 
-  it('deletes a note', () => {
-    cy.get('.list-group').contains('My note updated').click()
     cy.contains('Delete').click()
 
     cy.get('.list-group:contains(My note updated)').should('not.exist')
